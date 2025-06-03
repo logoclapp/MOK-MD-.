8394da763e73f8a233a48336d9aa3b4b340cc018
𝕸𝖔𝖐 𝕻𝖘𝖚𝖝𝖎𝖓 <mokpsuxin@gmail.com>
2025-06-03 17:15:22 -0400

    𝕸𝖔𝖐 𝕻𝖘𝖚𝖝𝖎𝖓

--- Plugins/dlt.js
+++ Plugins/dlt.js
@@ -7,13 +7,18 @@ bot(
     type: 'whatsapp',
   },
   async (message, match) => {
-    if (!message.reply_message) return await message.send(lang.plugins.common.reply_to_message)
+    if (!message.reply_message)
+      return await message.send('⚠️ *Reply to a message to delete.*')
+
     const key = message.reply_message.key
+
     if (!key.fromMe && message.isGroup) {
       const participants = await message.groupMetadata(message.jid)
       const isImAdmin = await isAdmin(participants, message.client.user.jid)
-      if (!isImAdmin) return await message.send(lang.plugins.common.not_admin)
+      if (!isImAdmin)
+        return await message.send('❌ *I need to be admin to delete others\' messages.*')
     }
+
     return await message.send(key, {}, 'delete')
   }
 )
