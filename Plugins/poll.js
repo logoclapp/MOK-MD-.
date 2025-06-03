dd9e208ed8d5f150be6de3cc2718122aefdc102b
𝕸𝖔𝖐 𝕻𝖘𝖚𝖝𝖎𝖓 <mokpsuxin@gmail.com>
2025-06-03 19:41:42 -0400

    ✪𝕸𝖔𝖐 𝕻𝖘𝖚𝖝𝖎𝖓 𒀱

--- Plugins/poll.js
+++ Plugins/poll.js
@@ -3,15 +3,24 @@ const { bot } = require('../lib/')
 bot(
   {
     pattern: 'poll ?(.*)',
-    desc: 'poll',
+    desc: 'Creates a WhatsApp poll.',
     type: 'whatsapp',
   },
   async (message, match) => {
     const poll = match.split(',')
-    if (poll.length < 3) return await message.send('*Example : question,option1,option2,...*')
+    if (poll.length < 3) {
+      return await message.send(
+        '╭──⭓ 𝙈𝙊𝙆 𝙈𝘿\n│ *Usage:* poll <question>,<option1>,<option2>,...\n╰────────────⭓'
+      )
+    }
+
     const name = poll[0]
     const options = []
-    for (let i = 1; i < poll.length; i++) options.push({ optionName: poll[i] })
+
+    for (let i = 1; i < poll.length; i++) {
+      options.push({ optionName: poll[i].trim() })
+    }
+
     await message.send(
       {
         name,
